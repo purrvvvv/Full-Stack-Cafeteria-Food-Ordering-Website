e87507f0a47fb7abf61983025d30cf861c315130
purrvvvv <purvshah10@gmail.com>
2024-01-10 15:23:59 +0530

    Orders page with admin access

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,41 @@
-import Notification from '@/components/Notification'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-import AuthProvider from '@/components/AuthProvider'
+import Notification from "@/components/Notification";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
+import QueryProvider from "@/components/QueryProvider";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Food delivering app',
-  description: 'BEST IN TOWN',
-}
+  title: "Food delivery app",
+  description: "Best food in town!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <AuthProvider>
-       <Notification/>
-       <Navbar/>
-        {children}
-        <Footer/>
-      </AuthProvider>
-        </body>
+        <AuthProvider>
+          <QueryProvider>
+            <div>
+              <Notification />
+              <Navbar />
+              {children}
+              <Footer />
+              <ToastContainer position="bottom-right" theme="dark" autoClose={3000}/>
+            </div>
+          </QueryProvider>
+        </AuthProvider>
+      </body>
     </html>
-  )
+  );
 }
